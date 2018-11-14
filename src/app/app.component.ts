cac758ffe4c4508fdaf639c2e6ac183bc159efb7
Renan Camillo - Note <rcamillo12@gmail.com>
2018-11-14 13:00:36 -0200

    Coreeção tela branca

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,58 +1,50 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+import { Component } from "@angular/core";
+import { Platform } from "ionic-angular";
+import { StatusBar } from "@ionic-native/status-bar";
+import { SplashScreen } from "@ionic-native/splash-screen";
 
-import { Observable} from 'rxjs';
+import { Observable } from "rxjs";
 
 import { AngularFireAuth } from "@angular/fire/auth";
 
-import { LoginPage } from '../pages/login/login';
-import { TabsPage } from '../pages/tabs/tabs';
+import { LoginPage } from "../pages/login/login";
+import { TabsPage } from "../pages/tabs/tabs";
 
 @Component({
-  templateUrl: 'app.html'
+  templateUrl: "app.html"
 })
 export class MyApp {
-  rootPage:any = null;
+  rootPage: any = null;
 
   public listagemUsuario: Observable<any[]>;
   public uiduser: string;
   public usuario: any;
 
-  constructor
-    (
-      platform: Platform, 
-      statusBar: StatusBar, 
-      splashScreen: SplashScreen,
-      public afAuth: AngularFireAuth,
-      ) 
-    {
-
-      platform.ready().then(() => {
-
-        afAuth.auth.onAuthStateChanged((user) => {
-
-          if(user != null) {
-              // está logado:
-            this.rootPage = TabsPage;
-              this.uiduser = this.afAuth.auth.currentUser.uid;
-              this.usuario = this.listagemUsuario;
-
-          }
-          else {
-              // não está logado:
-            this.rootPage = LoginPage;
-          }
-
-        });
-
-        // Okay, so the platform is ready and our plugins are available.
-        // Here you can do any higher level native things you might need.
-        statusBar.styleDefault();
-        splashScreen.hide();
+  constructor(
+    platform: Platform,
+    statusBar: StatusBar,
+    splashScreen: SplashScreen,
+    public afAuth: AngularFireAuth
+  ) {
+    platform.ready().then(() => {
+      afAuth.auth.onAuthStateChanged(user => {
+        if (user != null) {
+          // está logado:
+          this.rootPage = TabsPage;
+          statusBar.styleDefault();
+          splashScreen.hide(); // <-- aqui
+          this.uiduser = this.afAuth.auth.currentUser.uid;
+          this.usuario = this.listagemUsuario;
+        } else {
+          statusBar.styleDefault();
+          splashScreen.hide(); // <-- aqui
+          // não está logado:
+          this.rootPage = LoginPage;
+        }
       });
-    }
 
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+    });
+  }
 }
-
