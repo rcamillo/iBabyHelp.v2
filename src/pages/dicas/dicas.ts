c5aad3a6219f94d1baa641479bb1dfb14758f24e
rcamillo <42254362+rcamillo@users.noreply.github.com>
2018-11-17 17:31:45 -0200

    Correção
    Metodo Curtidas

--- src/pages/dicas/dicas.ts
+++ src/pages/dicas/dicas.ts
@@ -5,6 +5,8 @@ import { Observable } from "rxjs";
 import { AngularFireDatabase } from "angularfire2/database";
 
 import { AngularFireAuth } from "@angular/fire/auth";
+import { NgIf } from "@angular/common";
+import { THROW_IF_NOT_FOUND } from "@angular/core/src/di/injector";
 
 @Component({
   selector: "page-dicas",
@@ -12,32 +14,39 @@ import { AngularFireAuth } from "@angular/fire/auth";
 })
 export class DicasPage {
   public listaNoticia: Observable<any[]>;
-  // public listaQuemCurtiu: Observable<any[]>;
   public idusuario: string;
+  public teste;
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
     private db: AngularFireDatabase,
     public afAuth: AngularFireAuth
-  ) 
-  {
+  ) {
     this.idusuario = this.afAuth.auth.currentUser.uid;
     this.listaNoticia = db.list("/noticias/").valueChanges();
   }
 
   curtida(noticia: any) {
-    if (this.idusuario == noticia.quemCurtiu) {
+    if (noticia.curtidas == 0) {
       this.db
-      .list("/noticias/")
-      .update(noticia.id, { curtidas: noticia.curtidas - 1 });
-      this.db.list("/noticias/"+ noticia.id +"/quemCurtiu/").remove()
-    }else{
-      this.db
-      .list("/noticias/")
-      .update(noticia.id, { curtidas: noticia.curtidas + 1 });
-      this.db.database.ref("/noticias/"+ noticia.id).child("/quemCurtiu/").set(this.idusuario)
-    };
+        .list("/noticias/")
+        .update(noticia.id, { curtidas: noticia.curtidas + 1 });
+      this.db.database.ref("/noticias/" + noticia.id + "/quemCurtiu/").child(this.idusuario).set('')
+    } else {
+      this.teste = Object.keys(noticia.quemCurtiu)
+      if (this.idusuario == this.teste) {
+        this.db
+          .list("/noticias/")
+          .update(noticia.id, { curtidas: noticia.curtidas - 1 });
+        this.db.list("/noticias/" + noticia.id + "/quemCurtiu/").remove(this.idusuario);
+      } else {
+        this.db
+          .list("/noticias/")
+          .update(noticia.id, { curtidas: noticia.curtidas + 1 });
+        this.db.database.ref("/noticias/" + noticia.id + "/quemCurtiu/").child(this.idusuario).set('')
+      };
+    }
   }
 
   ionViewDidLoad() {
